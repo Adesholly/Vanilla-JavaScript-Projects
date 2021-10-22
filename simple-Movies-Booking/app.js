efcfa2c0ae88eda79a10ad514ff00630755abb71
adesoft <adesholly11@gmail.com>
2021-10-22 11:45:11 +0000

    storing movie

--- simple-Movies-Booking/app.js
+++ simple-Movies-Booking/app.js
@@ -2,26 +2,49 @@
 
 const movieEl = document.getElementById('movies');
 const containerEl = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
 const countEl = document.getElementById('count');
 const totalEl = document.getElementById('total');
 
+/*----X------------Getting HTML Element-----------X-------*/
 
+displayLS()
 
 //Storaging the selected movie and price into the local storage
+function setMoviesToLS(movieSeatIndex){
 
-
-
-function setMoviesToLS(movieIndex, moviePrice){
-    localStorage.setItem('selectedMovieIndex', movieIndex);
-    localStorage.setItem('selectedMoviePrice', moviePrice);
+    localStorage.setItem('selectedMovieIndex', movieSeatIndex);
+    
 }
+
+function displayLS(){
+
+    const selectedMovieSeat = JSON.parse(localStorage.getItem('selectedMovieSeat'));
+
+    if(selectedMovieSeat !== null && selectedMovieSeat.length > 0){
+        seats.forEach( (seat, index) => {
+
+            if(selectedMovieSeat.indexOf(index) !== -1){
+                seat.classList.add('selected');
+            }
+            
+        })
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if(selectedMovieIndex !== null){
+        movieEl.selectedIndex = selectedMovieIndex;
+    }
+}
+
 
 //Increasing the selected and price
 function updateCount(){
     const selectedSeat = document.querySelectorAll('.row .seat.selected')
-    
-    
 
+    const seatIndexes = [...selectedSeat].map( seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedMovieSeat', JSON.stringify(seatIndexes));
     
     const ticketPrice = +movieEl.value;
     const count = selectedSeat.length;
@@ -37,6 +60,7 @@ function updateCount(){
 /*------------------Adding Event Listeners---------------*/
 
 movieEl.addEventListener('change', e => {
+    setMoviesToLS(e.target.selectedIndex)
     updateCount();
 })
 
@@ -50,5 +74,5 @@ containerEl.addEventListener('click', e => {
         updateCount();
     }
 })
-
+updateCount();
 
